2dfb190a011e6734bf37c151cc9957ada0f15440
Tim Parsons <tim.parsons@cornell.edu>
2016-08-10 16:56:52 -0400

    Squashing bugs.  Injecting config into the GuiceInjectionProvider.

--- src/main/java/io/timparsons/bassoon/Application.java
+++ src/main/java/io/timparsons/bassoon/Application.java
@@ -17,6 +17,7 @@ import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.resource.ResourceCollection;
 import org.eclipse.jetty.util.security.Constraint;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.eclipse.jetty.util.thread.ScheduledExecutorScheduler;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,7 +37,11 @@ public abstract class Application<T extends Configuration> {
 
 	private Server server;
 
-	public void run(String ... args) {
+	public void run(String... args) {
+		run(null, args);
+	}
+
+	public <J extends GuiceInjectionProvider> void run(Class<J> injectionProvider, String... args) {
 		T config;
 		try {
 			config = getConfig(args);
@@ -44,151 +49,161 @@ public abstract class Application<T extends Configuration> {
 			log.error("Could not parse configuration file", e);
 			throw new RuntimeException(e);
 		}
-		
+
 		WebAppContext context = initJetty(config);
-		
-		initJSF(context, config);
-		
+
+		initJSF(context, config, injectionProvider);
+
 		initSecurity(context);
-		
+
 		try {
 			server.start();
+			log.info("Startup completed");
 		} catch (Exception e) {
 			log.error("Could not start the server", e);
 			throw new RuntimeException(e);
 		}
-		log.info("Startup completed");
-		
+
 		run(config);
-		
+
 		listen();
 	}
-	
+
 	protected abstract void run(T config);
 
 	private void listen() {
 		try {
 			server.join();
-		} catch(InterruptedException e) {
+		} catch (InterruptedException e) {
 			log.error("Runtime exception in server", e);
 		}
 	}
 
 	private void initSecurity(WebAppContext context) {
 		ConstraintSecurityHandler securityHandler = new ConstraintSecurityHandler();
-		
+
 		Constraint xhtmlConstraint = new Constraint();
 		xhtmlConstraint.setName("JSF Source Code Security Constraint");
 		xhtmlConstraint.setAuthenticate(true);
 		xhtmlConstraint.setRoles(null);
-		
+
 		ConstraintMapping xhtmlConstraintMapping = new ConstraintMapping();
 		xhtmlConstraintMapping.setPathSpec("*.xhtml");
 		xhtmlConstraintMapping.setConstraint(xhtmlConstraint);
 		securityHandler.setConstraintMappings(Collections.singletonList(xhtmlConstraintMapping));
-		
+
 		securityHandler.setHandler(context);
-		
+
 		server.setHandler(securityHandler);
 	}
 
-	private void initJSF(WebAppContext context, Configuration config) {
+	private <J extends GuiceInjectionProvider> void initJSF(WebAppContext context, T config,
+			Class<J> injectionProvider) {
 		log.info("initializing JSF");
-		
+
 		context.setInitParameter("com.sun.faces.forceLoadConfiguration", "true");
 		context.setInitParameter("com.sun.faces.enableRestoreView11Compatibility", "true");
-		
+
 		context.setInitParameter("javax.faces.PROJECT_STAGE", config.getProjectStage());
 		context.setInitParameter("javax.faces.FACELETS_SKIP_COMMENTS", "true");
 		context.setInitParameter("javax.faces.STATE_SAVING_METHOD", "server");
 		context.setInitParameter("javax.faces.DEFAULT_SUFFIX", ".xhtml");
-		
+
 		context.setInitParameter("defaultHtmlEscape", "true");
-		
-		for(Entry<String, String> initParam : config.getInitParams().entrySet()) {
-			context.setInitParameter(initParam.getKey(), initParam.getValue());
+
+		if (injectionProvider != null) {
+			GuiceInjectionProvider.setConfiguration(config);
+			context.setInitParameter("com.sun.faces.injectionProvider", injectionProvider.getName());
 		}
-		
+
+		if (config.getInitParams() != null) {
+			for (Entry<String, String> initParam : config.getInitParams().entrySet()) {
+				context.setInitParameter(initParam.getKey(), initParam.getValue());
+			}
+		}
+
 		context.addEventListener(new ConfigureListener());
-		
+
 		ServletHolder jsfServlet = new ServletHolder(FacesServlet.class);
 		jsfServlet.setDisplayName("Faces Servlet");
 		jsfServlet.setName("Faces_Servlet");
 		jsfServlet.setInitOrder(0);
-		
+
 		context.addServlet(jsfServlet, "*.jsf");
-		context.setWelcomeFiles(new String[] { "index.jsf"});
+		context.setWelcomeFiles(new String[] { "index.jsf" });
 	}
 
-	private WebAppContext initJetty(Configuration config) {
+	private WebAppContext initJetty(T config) {
 		LogManager.getLogManager().reset();
 		SLF4JBridgeHandler.install();
-		
+
 		log.info("Starting Jetty");
-		
+
 		QueuedThreadPool connectionThreadPool = new QueuedThreadPool();
 		connectionThreadPool.setName("JETTY_CONNECTIONS");
 		connectionThreadPool.setMinThreads(20);
 		connectionThreadPool.setMaxThreads(500);
 		connectionThreadPool.setStopTimeout(30 * 1000);
-		
+
 		server = new Server(connectionThreadPool);
+
+		server.addBean(new ScheduledExecutorScheduler());
 		server.setDumpAfterStart(false);
 		server.setDumpBeforeStop(false);
 		server.setStopAtShutdown(true);
-		
+
 		ServerConnector httpConnector = new ServerConnector(server);
-		if(config.getHost() != null) {
+		if (config.getHost() != null) {
 			httpConnector.setHost(config.getHost());
 		}
-		
+
 		httpConnector.setPort(config.getPort());
 		httpConnector.setIdleTimeout(config.getIdleTimeout());
 		server.addConnector(httpConnector);
-		log.info("HTTP Connector started on port: "+ config.getPort());
-		
+		log.info("HTTP Connector started on port: " + config.getPort());
+
 		String webappDir = this.getClass().getClassLoader().getResource("webapp").toExternalForm();
 		WebAppContext context = new WebAppContext(webappDir, config.getPath()) {
 			@Override
 			public String getResourceAlias(String alias) {
-				final Map<String, String> resourceAliases = (Map<String, String>) getResourceAliases();
-				
-				if(resourceAliases == null) {
+				final Map<String, String> resourceAliases = getResourceAliases();
+
+				if (resourceAliases == null) {
 					return null;
 				} else {
-					for(Entry<String, String> aliasEntry : resourceAliases.entrySet()) {
-						if(alias.startsWith(aliasEntry.getKey())) {
+					for (Entry<String, String> aliasEntry : resourceAliases.entrySet()) {
+						if (alias.startsWith(aliasEntry.getKey())) {
 							return alias.replace(aliasEntry.getKey(), aliasEntry.getValue());
 						}
 					}
-					
+
 					return null;
 				}
 			}
 		};
-		
+
 		try {
 			context.setBaseResource(new ResourceCollection(new String[] { webappDir, "./target" }));
-			context.setResourceAlias("/WEB-INF/classes", "/classes/");
-		} catch(Exception e) {
+			context.setResourceAlias("/WEB-INF/classes/", "/classes/");
+		} catch (Exception e) {
 			log.info("error setting base resource/alias");
 		}
-		
+
 		context.setInitParameter("org.eclipse.jetty.servlet.Default.dirAllowed", "false");
-		
+
 		context.setAttribute(SERVER_REFERENCE, this);
-		
-		log.info("Serving app from: "+webappDir);
-		
+
+		log.info("Serving app from: " + webappDir);
+
 		return context;
 	}
 
 	private T getConfig(String[] args) throws JsonParseException, JsonMappingException, IOException {
-		if(args.length == 0) {
+		if (args.length == 0) {
 			throw new IllegalArgumentException("Configuration file not specified");
 		}
 		File configFile = new File(args[0]);
-		
+
 		ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
 		return mapper.readValue(configFile, Generics.getTypeParameter(getClass(), Configuration.class));
 	}
--- src/main/java/io/timparsons/bassoon/GuiceInjectionProvider.java
+++ src/main/java/io/timparsons/bassoon/GuiceInjectionProvider.java
@@ -8,13 +8,13 @@ import com.google.inject.Module;
 import com.sun.faces.spi.InjectionProvider;
 import com.sun.faces.spi.InjectionProviderException;
 import com.sun.faces.vendor.WebContainerInjectionProvider;
- 
+
 /**
  * JSF injection provider for Guice.
+ * 
  * @author Joel Weight
  */
-public abstract class GuiceInjectionProvider implements InjectionProvider
-{
+public abstract class GuiceInjectionProvider implements InjectionProvider {
 	/**
 	 * default injector provided by the web container.
 	 */
@@ -25,41 +25,44 @@ public abstract class GuiceInjectionProvider implements InjectionProvider
 	 */
 	private static Injector injector;
 
-	private final List<Module> modules;
-	
-    protected GuiceInjectionProvider(List<Module> modules) {
-    	this.modules = modules;
-    }
- 
-    @Override
-    public void inject( Object managedBean ) throws InjectionProviderException
-    {
-        // allow the default injector to inject the bean.
-        con.inject( managedBean );
-        // then inject with the google injector.
-        getInjector().injectMembers( managedBean );
-    }
- 
-    private Injector getInjector() {
-    	if(injector == null) {
-    		injector = Guice.createInjector(modules);
-    	}
-    	
-    	return injector;
+	private static Configuration configuration;
+
+	@Override
+	public void inject(Object managedBean) throws InjectionProviderException {
+		// allow the default injector to inject the bean.
+		con.inject(managedBean);
+		// then inject with the google injector.
+		getInjector().injectMembers(managedBean);
+	}
+
+	private Injector getInjector() {
+		if (injector == null) {
+			injector = Guice.createInjector(getModules());
+		}
+
+		return injector;
+	}
+
+	protected abstract List<Module> getModules();
+
+	@Override
+	public void invokePostConstruct(Object managedBean) throws InjectionProviderException {
+		// don't do anything here for guice, just let the default do its thing
+		con.invokePostConstruct(managedBean);
 	}
 
 	@Override
-    public void invokePostConstruct( Object managedBean )
-            throws InjectionProviderException
-    {
-        // don't do anything here for guice, just let the default do its thing
-        con.invokePostConstruct( managedBean );
-    }
- 
-    @Override
-    public void invokePreDestroy( Object managedBean ) throws
-            InjectionProviderException
-    {
-        con.invokePreDestroy( managedBean );
-    }
+	public void invokePreDestroy(Object managedBean) throws InjectionProviderException {
+		con.invokePreDestroy(managedBean);
+	}
+
+	protected <T extends Configuration> T getConfiguration() {
+		return (T) configuration;
+	}
+
+	static void setConfiguration(Configuration configuration) {
+		if (GuiceInjectionProvider.configuration == null) {
+			GuiceInjectionProvider.configuration = configuration;
+		}
+	}
 }
